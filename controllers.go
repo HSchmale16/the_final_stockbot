ae77e231dae73e720dbc3261fb9bc6b8532f1daf
Henry Schmale <hschmale16@gmail.com>
2024-06-06 00:48:16 -0400

    More updates

--- controllers.go
+++ controllers.go
@@ -12,7 +12,7 @@ func GetLoadedArticlesStatus(c *gin.Context) {
 	db, _ := c.MustGet("db").(*gorm.DB)
 
 	var count int64
-	db.Model(&RSSItem{}).Where("DATE(published_date) >= DATE('now', '-2 days')").Count(&count)
+	db.Model(&RSSItem{}).Where("DATE(pub_date) >= DATE('now', '-2 days')").Count(&count)
 	c.JSON(http.StatusOK, gin.H{
 		"count": count,
 	})
