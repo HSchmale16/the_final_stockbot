d283252dfc8496e88e0733970710eab77b7b294a
Henry Schmale <hschmale16@gmail.com>
2024-06-18 23:19:14 -0400

    Stuff

--- prompt_manager.go
+++ prompt_manager.go
@@ -2,31 +2,25 @@ package main
 
 import (
 	"log"
-	"os"
+
+	_ "embed"
 
 	"gopkg.in/yaml.v3"
 )
 
 type Prompt struct {
-	// Define your configuration struct here
-	// Example:
-	// APIKey string `yaml:"api_key"`
-
 	Name       string `yaml:"name"`
 	PromptText string `yaml:"prompt_text"`
 }
 
+// go:embed prompts.yaml
+var yamlFile []byte
+
 var prompts []Prompt
 
 func init() {
-	// Read the YAML file
-	yamlFile, err := os.ReadFile("prompts.yaml")
-	if err != nil {
-		log.Fatalf("Failed to read YAML file: %v", err)
-	}
-
 	// Parse the YAML file into the Config struct
-	err = yaml.Unmarshal(yamlFile, &prompts)
+	err := yaml.Unmarshal(yamlFile, &prompts)
 	if err != nil {
 		log.Fatalf("Failed to parse YAML file: %v", err)
 	}
