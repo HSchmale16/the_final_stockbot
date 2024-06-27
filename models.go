b29b5bcc0d410bfca7e537c2db4f0b9fd4b17a0f
Henry Schmale <hschmale16@gmail.com>
2024-06-27 19:18:34 -0400

    More Stuff

--- models.go
+++ models.go
@@ -71,10 +71,6 @@ type GovtLawText struct {
 	GovtRssItem   GovtRssItem
 	Text          string
 	ModsXML       string
-
-	// Many to many relationship to represent the mods info
-	CongressMembers    []CongressPerson       `gorm:"many2many:govt_law_text_congress_member;"`
-	CongressCommittees []DB_CongressCommittee `gorm:"many2many:govt_law_text_congress_committee;"`
 }
 
 func (GovtLawText) TableName() string {
@@ -142,17 +138,6 @@ func (SearchQuery) TableName() string {
 	return "search_query"
 }
 
-// //////////////////////////////////////////////////////
-type CongressPerson struct {
-	ID uint
-	CongressMember
-}
-
-type DB_CongressCommittee struct {
-	ID uint
-	CongressCommittee
-}
-
 /**
  * Sets up the stupid database
  */
@@ -186,7 +171,7 @@ func setupDB() (*gorm.DB, error) {
 		return nil, err
 	}
 
-	if err := db.AutoMigrate(&SearchQuery{}, &DB_CongressCommittee{}, &CongressPerson{}); err != nil {
+	if err := db.AutoMigrate(&SearchQuery{}); err != nil {
 		return nil, err
 	}
 
