d9056b60e325dd26bbeca139fff150c4d2babfbc
Henry Schmale <hschmale16@gmail.com>
2024-07-07 19:02:33 -0400

    Get Lobbying Started

--- internal/lobbying/contribution.sql
+++ internal/lobbying/contribution.sql
@@ -11,18 +11,10 @@ from contributions, json_each(contributions.json_item, '$.contribution_items')
 where 
     json_item->>'no_contributions' = false
 )
--- select 
---     registrant_name
---     , printf('%,.2f', sum(cast(amount as float))) as total_amount
---     , count(*)
--- from unfolded
--- group by registrant_name;
-
 select 
-    filing_year,
     registrant_name
     , printf('%,.2f', sum(cast(amount as float))) as total_amount
     , count(*)
-from Test
-group by registrant_name, filing_year
+from unfolded
+group by filing_year, registrant_name
 order by sum(cast(amount as float)) DESC LIMIT 10;
--- internal/lobbying/routes.go
+++ internal/lobbying/routes.go
@@ -1,13 +1,56 @@
 package lobbying
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+	"gorm.io/gorm"
+)
 
 func SetupRoutes(app *fiber.App) {
-	app.Get("/lobbying", RenderLobbyingPage)
+	app.Get("/lobbying/:year", RenderLobbyingPage)
+}
+
+// [{"value":"feca","name":"FECA"},{"value":"he","name":"Honorary Expenses"},{"value":"me","name":"Meeting Expenses"},{"value":"ple","name":"Presidential Library Expenses"},{"value":"pic","name":"Presidential Inaugural Committee"}]%
+var ContributionType = map[string]string{
+	"feca": "FECA",
+	"he":   "Honorary Expenses",
+	"me":   "Meeting Expenses",
+	"ple":  "Presidential Library Expenses",
+	"pic":  "Presidential Inaugural Committee",
 }
 
 func RenderLobbyingPage(c *fiber.Ctx) error {
+	db := c.Locals("db").(*gorm.DB)
+	year := c.Params("year")
+
+	type Row struct {
+		ContributionType string `gorm:"column:contribution_type"`
+		Count            int
+		Amount           float64
+		AmountStr        string
+	}
+
+	var lobbyingContributions []Row
+
+	year2, _ := strconv.Atoi(year)
+	db.Raw("SELECT contribution_type, SUM(CAST(amount AS float)) Amount, Count(*) as Count FROM lobbyist_contributions WHERE filing_year = ? GROUP BY contribution_type", year2).Scan(&lobbyingContributions)
+	p := message.NewPrinter(language.English)
+
+	// iterate and update the contribution type with the diplay version
+	for i, row := range lobbyingContributions {
+		lobbyingContributions[i].ContributionType = ContributionType[row.ContributionType]
+		lobbyingContributions[i].AmountStr = p.Sprintf("%.2f", row.Amount)
+	}
+
+	fmt.Println(year, lobbyingContributions)
+
 	return c.Render("lobbying", fiber.Map{
-		"Title": "Lobbying",
-	})
+		"Title": "Lobbying Spending for " + c.Params("year"),
+		"Year":  c.Params("year"),
+		"Data":  lobbyingContributions,
+	}, "layouts/main")
 }
