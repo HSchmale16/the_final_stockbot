8bde17cba63b76fa5d6eee43b76eb9c6a2e0d23e
Henry Schmale <hschmale16@gmail.com>
2024-07-30 17:46:22 -0400

    even more get there

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+	  "base.css",
     "internal/app/html_templates/**/*.hbs",
     "internal/app/html_templates/*.hbs"
   ],
