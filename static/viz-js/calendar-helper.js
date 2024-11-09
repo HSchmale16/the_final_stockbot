8eab8b078c6240dd7eb8773a9490052230bef58b
Henry Schmale <hschmale16@gmail.com>
2024-11-09 15:35:36 -0500

    make better things

--- static/viz-js/calendar-helper.js
+++ static/viz-js/calendar-helper.js
@@ -2,11 +2,30 @@
  * Helps with the travel calendar to provide better rendering of things
  */
 
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function () {
     const travelDays = document.querySelectorAll('.travelDay');
 
     var grid = new gridjs.Grid({
-        columns: ['Filer Name', 'Departure Date', 'Return Date', 'Destination', 'Congress Person'],
+        columns: [
+            'Filer Name',
+            {
+                name: 'Departure Date',
+                formatter: (cell) => {
+                    return new Date(cell).toLocaleDateString();
+                }
+            },
+            {
+                name: 'Return Date',
+                formatter: (cell) => {
+                    return new Date(cell).toLocaleDateString();
+                }
+            }, 
+            'Destination',
+            {
+                name: 'Congress Person',
+            }
+        ],
         server: {
             url: `/json/travel/calendar/${yearInt}/${monthInt}`,
             then: data => data.map(row => [
@@ -18,14 +37,15 @@ document.addEventListener('DOMContentLoaded', function() {
             ])
         }
     })
-    
+    console.log('grid', grid);
+
     grid.render(document.getElementById('wrapper'));
 
 
-    travelDays.forEach(function(day) {
+    travelDays.forEach(function (day) {
         const monthInt = parseInt(day.getAttribute('data-month'));
         const dayInt = parseInt(day.innerText);
-        day.addEventListener('click', function() {
+        day.addEventListener('click', function () {
             console.log('clicked on day', dayInt, monthInt);
         });
     });
