5ff05e912b656df034b017c1b9febdb5339b115d
Henry Schmale <hschmale16@gmail.com>
2025-05-11 22:12:57 -0400

    more updates

--- cron-scripts/house-gift-travel.sh
+++ cron-scripts/house-gift-travel.sh
@@ -5,18 +5,24 @@ pwd
 YEAR=$(date +"%Y")
 if [ -n "$1" ]; then
     YEAR=$1
+else
+    echo "FAILED TO GET YEAR"
+    exit 1
 fi
-wget https://disclosures-clerk.house.gov/public_disc/gift-pdfs/${YEAR}Travel.zip
+
+filePath=$(mktemp)
+
+wget -O $filePath https://disclosures-clerk.house.gov/public_disc/gift-pdfs/${YEAR}Travel.zip
 
 if [[ ! -x ./the_final_stockbot/the_final_stockbot ]]; then
 
     if [[ ! -f ./the_final_stockbot ]]; then
         echo "Error: the_final_stockbot binary not found."
     else
-        ./the_final_stockbot -script house-travel -file ${YEAR}Travel.zip
+        ./the_final_stockbot -script house-travel -file $filePath
     fi
 
 else
-    ./the_final_stockbot/the_final_stockbot -script house-travel -file ${YEAR}Travel.zip
+    ./the_final_stockbot/the_final_stockbot -script house-travel -file $filePath
 fi
-rm ${YEAR}Travel.zip
+rm $filePath
